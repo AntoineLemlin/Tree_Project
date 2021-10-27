cda2d81a97f5e77abb0df57b9bb08648fe6fb440
AntoineLemlin <ant.lemlin@gmail.com>
2021-10-27 16:32:52 +0200

    fix compatibility webpack

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,9 +1,45 @@
 import * as React from "react";
+import logo from "../img/logo.png";
+import menu from "../img/menu_hamburger.png";
 
-const Header = () => (
-    <header>
-        <h1>{"Treedle.io"}</h1>
-    </header>
-);
+const Header = () => {
+    const navbar = React.useRef();
+    const isMenu = () => {
+        navbar.current.style.visibility === "visible"
+            ? (navbar.current.style.visibility = "hidden")
+            : (navbar.current.style.visibility = "visible");
+
+        console.log(navbar.current.style.visibility);
+    };
+
+    return (
+        <header>
+            <div className="logo">
+                <img src={logo} alt="" />
+            </div>
+
+            <h1>{"Treedle.io"}</h1>
+
+            <div className="links">
+                <nav style={{visibility: "visible"}} ref={navbar}>
+                    <ul>
+                        <li>
+                            <a href="#">Profile</a>
+                        </li>
+                        <li>
+                            <a href="#">Logout</a>
+                        </li>
+                    </ul>
+                </nav>
+
+                <div className="menu">
+                    <a href="#" onClick={isMenu}>
+                        <img src={menu} alt="" />
+                    </a>
+                </div>
+            </div>
+        </header>
+    );
+};
 
 export default Header;
