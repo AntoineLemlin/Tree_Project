48bded7edf48c98b543f98d5577f354ea6d2587a
AntoineLemlin <ant.lemlin@gmail.com>
2021-10-28 12:30:56 +0200

    coordinates map

--- src/client/components/Map.js
+++ src/client/components/Map.js
@@ -1,18 +1,17 @@
 import * as React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import {MapContainer, TileLayer} from "react-leaflet";
 const Map = () => (
     <div className="map-liege">
-        <MapContainer className="map-layout" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-  <TileLayer
-    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker position={[51.505, -0.09]}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker>
-</MapContainer>
+        <MapContainer
+            className="map-layout"
+            center={[50.6385, 5.5811]}
+            zoom={13}
+            scrollWheelZoom={true}>
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+        </MapContainer>
     </div>
 );
 
