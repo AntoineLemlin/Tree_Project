e95543979cd43fc1405a7c8b55d6babc3c697138
doxh23 <adrien.peters95@gmail.com>
2021-10-28 11:24:50 +0200

    add map

--- src/client/app.js
+++ src/client/app.js
@@ -1,9 +1,10 @@
 import * as React from "react";
-
+import Map from './components/Map'
 const App = () => {
     return (
         <div className={"app"}>
             <h1>{"Hello"}</h1>
+            <Map />
         </div>
     );
 };
