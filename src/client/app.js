b4e1f7c7d6403ff25c447fdb0a086e290201f72a
doxh23 <adrien.peters95@gmail.com>
2021-10-29 16:28:06 +0200

    login form + design

--- src/client/app.js
+++ src/client/app.js
@@ -2,10 +2,11 @@ import * as React from "react";
 import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
 import Map from "./components/Map";
-
+import Login from "./components/login";
 const App = () => {
     return (
         <div className={"app"}>
+            <Login />
             <Header />
             <main>
                 <Map />
---
+++ src/client/components/Login.js
@@ -0,0 +1,33 @@
+import * as React from "react";
+
+const Login = () => {
+    return (
+        <div className="login-register">
+           <div className="menu-login-register">
+            <div className="login">
+               <h2>login</h2>
+               </div>
+               <div className="register">
+               <h2>register</h2>
+               </div>
+           </div>
+        <div className="login-field">
+        <form>
+        <label for="username"> username</label>
+    
+        <input type="text" id="username" name="username" ></input>
+        <label for="password">Password </label>
+        <input type="text"id="password" name="password"></input>
+        <button>Login</button>
+        </form>
+      
+
+        </div>
+        </div>
+
+
+
+    );
+};
+
+export default Login;
