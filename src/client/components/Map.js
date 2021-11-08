ee88c996d3ddf92c782a5df9e9cedc0a2a717229
doxh23 <adrien.peters95@gmail.com>
2021-11-08 15:59:23 +0100

    add svg to map

--- src/client/components/Map.js
+++ src/client/components/Map.js
@@ -1,6 +1,17 @@
 import * as React from "react";
-import {MapContainer, TileLayer} from "react-leaflet";
+import {MapContainer, TileLayer,Marker,Popup} from "react-leaflet";
+import L ,{iconSize} from "leaflet"
+import tree1 from "../img/tree-svgrepo-com.svg"
+function test(_iconSize){
+    return L.icon({
+        iconUrl:tree1,
+        iconSize: [_iconSize]
+    })
+}
+
+ 
 const Map = () => (
+   
     <div className="map-liege">
         <MapContainer
             className="map-layout"
@@ -8,6 +19,13 @@ const Map = () => (
             zoom={13}
             scrollWheelZoom={true}
             zoomControl={false}>
+            <Marker  icon={test(80)} position={[50.6385, 5.5811]}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+
+
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
