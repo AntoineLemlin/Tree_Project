78824a0b56136a589c555fc81e7d1ccd50635ced
MrPinoBoy <beaupain.thomas@gmail.com>
2021-11-08 12:01:00 +0100

    added connection to the mongodb database

--- src/server/index.js
+++ src/server/index.js
@@ -8,6 +8,9 @@
 
 import express from "express";
 import path from "path";
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://thomas-b:zBaiQVtqyzN7eZqw@cluster0.0xebq.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const {APP_PORT} = process.env;
 
@@ -16,9 +19,16 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, "../../bin/client")));
 
 app.get("/hello", (req, res) => {
-    console.log(`ℹ️  (${req.method.toUpperCase()}) ${req.url}`);
-    res.send("Hello, World!");
+client.connect(err => {
+  const collection = client.db("DataTree").collection("Trees");
+  collection.findOne({}, (err,result) => {
+      if (err) throw err;
+      console.log(result.geoloc);
+      client.close();
+  })
 });
+});
+
 
 app.listen(APP_PORT, () =>
     console.log(`🚀 Server is listening on port ${APP_PORT}.`),
