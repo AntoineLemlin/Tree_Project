9845a8d2c67fcce6d3c059afe88e1565ca93dc1e
AntoineLemlin <ant.lemlin@gmail.com>
2021-11-09 17:08:15 +0100

    loader added fetch

--- src/client/app.js
+++ src/client/app.js
@@ -1,4 +1,6 @@
 import React, {useEffect,useState} from "react";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
 import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
 import Carte from "./components/Map";
@@ -9,19 +11,21 @@ import axios from "axios";
 const App = () => {
     
 const [trees, setTrees] = useState({})
-const [fetch, setFetch] = useState(false)
+const [loading, setLoading] = useState(true)
 
 const getTrees = () => {
     axios.get("/api/tree")
     .then(res => {
         const trees = res.data;
         setTrees(trees);
-        setFetch(true);
+        setLoading(false)
     })
 }
 
 useEffect(()=>{
+    { 
     getTrees()
+    }
 }, [])
 
     return (
@@ -29,9 +33,22 @@ useEffect(()=>{
             <Login />
             <Header />
             <main>
-                <Carte listTrees={trees}/>
-                <Sidebar />
-                <Statbar />
+                {loading ?  
+                <Loader
+                type="Oval"
+                color="#00BFFF"
+                height={100}
+                width={100}
+                /> 
+                : 
+                <>
+                    <Carte listTrees={trees}/>
+                    <Sidebar />
+                    <Statbar />
+                </>
+                }
+                
+                
             </main>
         </div>
     );
