c75a05cfce02d41e8f75cd9e629ebfbe5986ae57
MrPinoBoy <beaupain.thomas@gmail.com>
2021-11-10 14:25:49 +0100

    mapscreen show up

--- src/client/app.js
+++ src/client/app.js
@@ -1,9 +1,10 @@
 import React from "react";
 import AppRouter from "./routers/AppRouter";
+import MapScreen from "./screens/MapScreen";
 
 const App = () => {
     return ( 
-        <AppRouter/>
+        <MapScreen/>
      );
 }
  
