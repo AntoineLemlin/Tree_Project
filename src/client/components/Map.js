7d22870f83b233cb95d0d91affd54806951c7f90
MrPinoBoy <beaupain.thomas@gmail.com>
2021-11-10 10:45:48 +0100

    changed icons id and icon sizes

--- src/client/components/Map.js
+++ src/client/components/Map.js
@@ -22,9 +22,9 @@ const Carte = (props) => {
                     position: [tree.geoloc.lat, tree.geoloc.lon],
                     onClick: () => alert(`${tree.nom_complet}`),
                     tooltip: `${tree.nom_complet}`,
-                    id: i,
-                    iconId: Math.random()*30000,
-                    customIcon:  `<svg width="52" height="62" viewBox="0 0 52 62" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    id: `${i}`,
+                    iconId: `icon-${i}`,
+                    customIcon:  `<svg width="26" height="31" viewBox="0 0 26 31" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M26 20C22.5787 40.5344 17.2282 55.372 11 60.2587V62H26V20Z" fill="#5E4D3E"/>
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M26 20C29.4213 40.5344 34.7718 55.372 41 60.2587V62H26V20Z" fill="#5E4D3E"/>
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M41.1523 10.8477C39.0078 4.53895 33.034 0 26 0C18.966 0 12.9922 4.53895 10.8477 10.8477C4.53895 12.9922 0 18.966 0 26C0 34.8366 7.16344 42 16 42C19.7838 42 23.2608 40.6866 26 38.4907C28.7392 40.6866 32.2162 42 36 42C44.8366 42 52 34.8366 52 26C52 18.966 47.4611 12.9922 41.1523 10.8477Z" fill="#A43B3B"/>
