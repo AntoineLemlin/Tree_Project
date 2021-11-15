eaa3fd17da1906b2785700acb0acf9f3829330f4
AntoineLemlin <ant.lemlin@gmail.com>
2021-11-15 11:59:19 +0100

    trees in  radius

--- src/client/routers/AppRouter.js
+++ src/client/routers/AppRouter.js
@@ -1,17 +1,20 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import {HashRouter, Route, Routes} from "react-router-dom";
 import Login from "../components/Login";
 import LoginScreen from "../screens/LoginScreen";
 import MapScreen from "../screens/MapScreen";
 
 const AppRouter = () => {
-    const [user, setLoginUser] = useState({})
+    const [user, setLoginUser] = useState({});
     return (
         <HashRouter>
             <div>
                 <Routes>
-                    <Route path="/" element={<LoginScreen/>}/>
-                    <Route path="/login" element={<LoginScreen setLoginUser={setLoginUser}/>} />
+                    <Route path="/" element={<MapScreen />} />
+                    <Route
+                        path="/login"
+                        element={<LoginScreen setLoginUser={setLoginUser} />}
+                    />
                 </Routes>
             </div>
         </HashRouter>
